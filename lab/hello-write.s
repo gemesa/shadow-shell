dfc65ab1e78b162707746db6531bb89d842a8952
Andras Gemes <andrasgemes@outlook.com>
2023-11-04 17:27:46 +0100

    docs: update lab code docs

--- lab/hello-write.s
+++ lab/hello-write.s
@@ -9,7 +9,7 @@ message:
 _start:
 
     mov $1, %rax        ; syscall NR - write: 1
-    mov $1, %rdi        ; arg0 - unsigned int fd - standard output: 1
+    mov $1, %rdi        ; arg0 - unsigned int fd - stdout: 1
     mov $message, %rsi  ; arg1 - const char *buf
     mov $6, %rdx        ; arg2 - size_t count
     syscall
