80d5c6e0c658f50fe88b5336ba12c1c2762f7ff5
Andras Gemes <andrasgemes@outlook.com>
2023-11-04 17:49:47 +0100

    style: asmfmt lab codes

--- lab/crt-hello.s
+++ lab/crt-hello.s
@@ -1,14 +1,14 @@
-.section .note.GNU-stack,"",@progbits
+.section .note.GNU-stack, "", @progbits
 
 .global main
 
 .text
 
 main:
-    mov $message, %rdi  # arg0 - const char *string
-    call puts
-    xor %eax, %eax      # set return value to 0
-    ret
+	mov  $message, %rdi  # arg0 - const char *string
+	call puts
+	xor  %eax, %eax      # set return value to 0
+	ret
 
 message:
-    .ascii "Hello\n"
+	.ascii "Hello\n"
--- lab/nocrt-hello.s
+++ lab/nocrt-hello.s
@@ -1,19 +1,20 @@
 .global _start
 
 .data
-message:
-    .ascii "Hello\n"
 
-.text
+message:
+	.ascii "Hello\n"
+
+	.text
 
 _start:
 
-    mov $1, %rax        # syscall NR - write: 1
-    mov $1, %rdi        # arg0 - unsigned int fd - stdout: 1
-    mov $message, %rsi  # arg1 - const char *buf
-    mov $6, %rdx        # arg2 - size_t count
-    syscall
+	mov $1, %rax        # syscall NR - write: 1
+	mov $1, %rdi        # arg0 - unsigned int fd - stdout: 1
+	mov $message, %rsi  # arg1 - const char *buf
+	mov $6, %rdx        # arg2 - size_t count
+	syscall
 
-    mov $60, %rax       # syscall NR - exit: 60
-    xor %rdi, %rdi      # arg0 - int error_code
-    syscall
+	mov $60, %rax       # syscall NR - exit: 60
+	xor %rdi, %rdi      # arg0 - int error_code
+	syscall
