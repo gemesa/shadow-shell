df7114927cc7f2eb7bb9699115574922164aea22
Andras Gemes <andrasgemes@outlook.com>
2023-11-04 17:45:16 +0100

    docs: update lab code docs

--- lab/crt-hello.s
+++ lab/crt-hello.s
@@ -5,9 +5,9 @@
 .text
 
 main:
-    mov $message, %rdi  ; arg0 - const char *string
+    mov $message, %rdi  # arg0 - const char *string
     call puts
-    xor %eax, %eax      ; set return value to 0
+    xor %eax, %eax      # set return value to 0
     ret
 
 message:
--- lab/nocrt-hello.s
+++ lab/nocrt-hello.s
@@ -8,12 +8,12 @@ message:
 
 _start:
 
-    mov $1, %rax        ; syscall NR - write: 1
-    mov $1, %rdi        ; arg0 - unsigned int fd - stdout: 1
-    mov $message, %rsi  ; arg1 - const char *buf
-    mov $6, %rdx        ; arg2 - size_t count
+    mov $1, %rax        # syscall NR - write: 1
+    mov $1, %rdi        # arg0 - unsigned int fd - stdout: 1
+    mov $message, %rsi  # arg1 - const char *buf
+    mov $6, %rdx        # arg2 - size_t count
     syscall
 
-    mov $60, %rax       ; syscall NR - exit: 60
-    xor %rdi, %rdi      ; arg0 - int error_code
+    mov $60, %rax       # syscall NR - exit: 60
+    xor %rdi, %rdi      # arg0 - int error_code
     syscall
