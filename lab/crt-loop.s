3d7789faa92f636538b4b7f0112f5bc336403cb7
Andras Gemes <andrasgemes@outlook.com>
2023-11-05 20:23:51 +0100

    feat: add loop lab code

--- lab/crt-loop.s
+++ lab/crt-loop.s
@@ -5,4 +5,24 @@
 .text
 
 main:
+	push %rbp
+	mov  %rsp, %rbp
+	movl $0, -4(%rbp)
+	sub  $16, %rsp
+
+loop_start:
+	mov  $message, %edi
+	call puts
+	cmpl $9, -4(%rbp)
+	je   loop_end
+	addl $1, -4(%rbp)
+	jmp  loop_start
+
+loop_end:
+	mov %rbp, %rsp
+	pop %rbp
+    xor %eax, %eax
 	ret
+
+message:
+	.ascii "iter\0"
