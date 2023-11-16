e24d28cef4dd9a2a8ff42cc660da79cf959da3b3
Andras Gemes <andrasgemes@outlook.com>
2023-11-16 14:55:03 +0100

    refactor: add intel version of jmp func lab code

--- lab/nocrt-jmp-func.s
+++ lab/nocrt-jmp-func.s
@@ -2,18 +2,23 @@
 
 .text
 
-_start:
+message:
+	.ascii "Hello\n"
+
+.att_syntax
+
+_start_:
 	lea  exit(%rip), %rax
 	push %rax
 
 	jmp print
 
-exit:
+exit_:
 	mov $60, %rax       # syscall NR - exit: 60
 	xor %rdi, %rdi      # arg0 - int error_code
 	syscall
 
-print:
+print_:
 	mov $1, %rax        # syscall NR - write: 1
 	mov $1, %rdi        # arg0 - unsigned int fd - stdout: 1
 	mov $message, %rsi  # arg1 - const char *buf
@@ -23,5 +28,25 @@ print:
 	pop %rax
 	jmp *%rax
 
-message:
-	.ascii "Hello\n"
+.intel_syntax noprefix
+
+_start:
+	lea  rax, [ rip + exit]
+	push rax
+
+	jmp print
+
+exit:
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
+print:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, offset message
+	mov rdx, 6
+	syscall
+
+	pop rax
+	jmp rax
