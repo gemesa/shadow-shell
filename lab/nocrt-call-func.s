0ccd739e7f032958f39fa47e0e958fa96ec50a28
Andras Gemes <andrasgemes@outlook.com>
2023-11-16 14:54:16 +0100

    refactor: add intel version of call func lab code

--- lab/nocrt-call-func.s
+++ lab/nocrt-call-func.s
@@ -2,15 +2,20 @@
 
 .text
 
-_start:
+message:
+	.ascii "Hello\n"
+
+.att_syntax
+
+_start_:
 	call print
 
-exit:
+exit_:
 	mov $60, %rax       # syscall NR - exit: 60
 	xor %rdi, %rdi      # arg0 - int error_code
 	syscall
 
-print:
+print_:
 	mov $1, %rax        # syscall NR - write: 1
 	mov $1, %rdi        # arg0 - unsigned int fd - stdout: 1
 	mov $message, %rsi  # arg1 - const char *buf
@@ -19,5 +24,21 @@ print:
 
 	ret
 
-message:
-	.ascii "Hello\n"
+.intel_syntax noprefix
+
+_start:
+	call print
+
+exit:
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
+print:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, offset message
+	mov rdx, 6
+	syscall
+
+	ret
