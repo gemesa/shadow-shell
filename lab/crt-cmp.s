0df84cb8a6671c27958a507e5b70937a06427bed
Andras Gemes <andrasgemes@outlook.com>
2023-11-17 20:21:04 +0100

    refactor: add intel version of crt cmp lab code

--- lab/crt-cmp.s
+++ lab/crt-cmp.s
@@ -4,7 +4,12 @@
 
 .text
 
-main:
+.att_syntax
+
+format_string:
+	.asciz "%d\n"
+
+main_:
 	push %rbp
 	mov  $1, %edi
 	mov  $2, %esi
@@ -16,19 +21,44 @@ main:
 	pop  %rbp
 	ret
 
-min:
+min_:
 	push %rbp
 	mov  %rsp, %rbp
 	cmp  %esi, %edi # edi-esi = 1-2, eflags = [ CF PF AF SF IF ], if edi-esi = 2-1 --> eflags = [ IF ], if edi-esi = 1-1 --> eflags = [ PF ZF IF ]
 	jb   below
 	mov  %esi, %eax
 
-below:
+below_:
 	mov %edi, %eax
 
 	mov %rbp, %rsp
 	pop %rbp
 	ret
 
-format_string:
-	.asciz "%d\n"
+.intel_syntax noprefix
+
+main:
+	push rbp
+	mov  edi, 1
+	mov  esi, 2
+	call min
+	mov  edi, offset format_string
+	mov  esi, eax
+	call printf
+	xor  eax, eax
+	pop  rbp
+	ret
+
+min:
+	push rbp
+	mov  rbp, rsp
+	cmp  edi, esi
+	jb   below
+	mov  eax, esi
+
+below:
+	mov eax, edi
+
+	mov rsp, rbp
+	pop rbp
+	ret
