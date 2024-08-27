eac15e3b1f7ac4c110f3305dd3cca19dc4f6748c
Andras Gemes <andrasgemes@outlook.com>
2024-08-27 19:26:07 +0200

    refactor: replace syscalls with libc calls in shexec.s

--- arsenal/shexec.s
+++ arsenal/shexec.s
@@ -5,19 +5,19 @@
 usage:
 	.asciz "Usage: %s <shellcode_file>\n"
 
-open:
+open_msg:
 	.asciz "open"
 
-fstat:
+fstat_msg:
 	.asciz "fstat"
 
-mmap:
+mmap_msg:
 	.asciz "mmap"
 
-read:
+read_msg:
 	.asciz "read"
 
-mprotect:
+mprotect_msg:
 	.asciz "mprotect"
 
 filesize_fmt:
@@ -34,26 +34,21 @@ main:
 	cmp r12, 2
 	jne args_failure
 
-	call __errno_location
-	mov r15, rax		# address of errno
-
-	mov rax, 2 			# syscall NR - open: 2
+	xor rax, rax        # clear rax before calling variadic function
 	mov rdi, [r13 + 8]	# arg0 - const char *filename
 	mov rsi, 0			# arg1 - int flags
-	xor rdx, rdx		# arg2 - umode_t mode
-	syscall
+	call open
 
-	test rax, rax
-	js open_failure
+	cmp eax, -1
+	je open_failure
 
 	mov r14, rax		# fd
 
 	sub rsp, 144		# size of stat struct, refer to struct stat in /usr/include/bits/struct_stat.h
 
-	mov rax, 5			# syscall NR - fstat: 5
 	mov rdi, r14		# arg0 - unsigned int fd
 	mov rsi, rsp
-	syscall
+	call fstat
 
 	test rax, rax
 	js fstat_failure
@@ -65,7 +60,6 @@ main:
     xor rax, rax        # clear rax before calling variadic function
     call printf
 
-	mov rax, 9			# syscall NR - mmap: 9
 	xor rdi, rdi		# arg0 - void* addr
 	mov rsi, [rsp + 48]	# arg1 - size_t len
 	# https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/mman-common.h
@@ -82,26 +76,23 @@ main:
 	or rcx, 0x2
 	mov r8, -1			# arg3 - int fd
 	xor r9, r9			# arg4 - off_t offset
-	syscall
+	call mmap
 
 	test rax, rax
 	js mmap_failure
 
 	mov r12, rax		# mmap addr
-	mov rax, 0			# syscall NR - read: 0
 	mov rdi, r14		# arg0 - unsigned int fd
 	mov rsi, r12		# arg1 - char *buf
 	mov rdx, [rsp + 48]	# arg2 - size_t count
-	syscall
+	call read
 
 	test rax, rax
 	js read_failure
 
 	mov rdi, r14		# arg0 - unsigned int fd
-	mov rax, 3 			# syscall NR - close: 3
-	syscall
+	call close
 
-	mov rax, 10			# syscall NR - mprotect: 10
 	mov rdi, r12		# arg0 - unsigned long start
 	mov rsi, [rsp + 48]	# arg1 - size_t len
 	# https://github.com/torvalds/linux/blob/master/include/uapi/asm-generic/mman-common.h
@@ -111,7 +102,7 @@ main:
 	mov rdx, 0x1		# arg2 - unsigned long prot
 	or rdx, 0x2
 	or rdx, 0x4
-	syscall
+	call mprotect
 
 	test rax, rax
 	js mprotect_failure
@@ -135,37 +126,27 @@ args_failure:
 	jmp exit_failure
 
 open_failure:
-	neg rax
-	mov [r15], eax		# `errno` is a 32 bit int: https://man7.org/linux/man-pages/man3/errno.3.html
-	mov rdi, offset open
+	mov rdi, offset open_msg
 	call perror
 	jmp exit_failure
 
 fstat_failure:
-	neg rax
-	mov [r15], eax		# `errno` is a 32 bit int: https://man7.org/linux/man-pages/man3/errno.3.html
-	mov rdi, offset fstat
+	mov rdi, offset fstat_msg
 	call perror
 	jmp exit_failure
 
 mmap_failure:
-	neg rax
-	mov [r15], eax		# `errno` is a 32 bit int: https://man7.org/linux/man-pages/man3/errno.3.html
-	mov rdi, offset mmap
+	mov rdi, offset mmap_msg
 	call perror
 	jmp exit_failure
 
 read_failure:
-	neg rax
-	mov [r15], eax		# `errno` is a 32 bit int: https://man7.org/linux/man-pages/man3/errno.3.html
-	mov rdi, offset read
+	mov rdi, offset read_msg
 	call perror
 	jmp exit_failure
 
 mprotect_failure:
-	neg rax
-	mov [r15], eax		# `errno` is a 32 bit int: https://man7.org/linux/man-pages/man3/errno.3.html
-	mov rdi, offset mprotect
+	mov rdi, offset mprotect_msg
 	call perror
 	jmp exit_failure
 
